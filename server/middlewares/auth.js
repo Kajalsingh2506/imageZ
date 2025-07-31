624f83eca9e1be22e5ca5f6ad0c1199d43a4de88
Kajal Singh <kajalsingh47428@gmail.com>
2025-07-31 13:41:45 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,27 +1,27 @@
-import jwt from 'jsonwebtoken';
+
+import jwt from 'jsonwebtoken'
 
 // user authentication middleware
 const authUser = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.json({ success: false, message: "Not Authorized Login Again" });
+    const { token } = req.headers
+    if (!token) {
+        return res.json({ success: false, message: 'Not Authorized Login Again' })
     }
+    try {
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
 
-    const token = authHeader.split(" ")[1]; // extract the token only
-    const token_decode = jwt.verify(token, process.env.JWT_SECRET); // verify token
+        if (token_decode.id) {
+            req.body.userId = token_decode.id;
+        }else{
+            return res.json({ success: false, message: 'Not Authorized Login Again' }) 
+        }
 
-    if (token_decode.id) {
-      req.body.userId = token_decode.id; // put userId in request body
-      next(); // move to next function
-    } else {
-      return res.json({ success: false, message: "Not Authorized Login Again" });
+        next();
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false, message: error.message });
-  }
-};
+}
 
 export default authUser;
